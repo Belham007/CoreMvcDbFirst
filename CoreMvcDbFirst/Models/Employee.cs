b8dac16999eb9178906b6dcf82a5f5a74ea987cf
Deepak kumar <deepakdare@outlook.com>
2023-09-06 21:51:33 +0530

    Add project files.

---
+++ CoreMvcDbFirst/Models/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CoreMvcDbFirst.Models;
+
+public partial class Employee
+{
+    public int Id { get; set; }
+
+    public string FirstName { get; set; } = null!;
+
+    public string LastName { get; set; } = null!;
+
+    public DateTime DateOfBirth { get; set; }
+
+    public string Email { get; set; } = null!;
+
+    public double Salary { get; set; }
+
+    public string Remark { get; set; } = null!;
+}
